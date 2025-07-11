4def101d67eee493a23bfa670ede2d35d80008e0
monishajadav <jadavmonisha418.gmail.com>
2025-07-11 20:55:23 +0530

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+    <footer class="footer mt-auto">
+        <div class="container text-center">
+            <div class="mb-2">
+                <a href="#" class="text-white me-3"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="text-white me-3"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="text-white me-3"><i class="bi bi-instagram"></i></a>
+            </div>
+            <div>
+                &copy; 2024 E-Commerce. All rights reserved.
+            </div>
+        </div>
+    </footer>
